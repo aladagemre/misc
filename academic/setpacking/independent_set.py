1c72bdf657382a11cc56c12e39c4e02f50c5915c
Ahmet Emre Aladağ <aladagemre@gmail.com>
2011-05-04 17:09:28 +0300

    Improvement part added but alpha-improvement values are always < 1. So no improvement is done.

--- academic/setpacking/independent_set.py
+++ academic/setpacking/independent_set.py
@@ -1,5 +1,9 @@
 import os,random
+import itertools
 
+K=2
+
+		
 class Node:
 	def __init__(self, items=None, weight=None):
 		if not items:
@@ -17,7 +21,8 @@ class Node:
 	
 	def __str__(self):
 		return "%s (%f)" % ( "".join(self.items), self.weight )
-		
+	def __repr__(self):
+		return self.__str__()
 	def __cmp__(self, other):
 		if self.items == other.items:
 			return 0
@@ -62,7 +67,7 @@ class SetGraph:
 			G.add_node(node)
 		for edge in self.edges:
 			G.add_edge(edge[0], edge[1], color='blue')
-			print "Adding %s %s" % (edge[0], edge[1])
+			#print "Adding %s %s" % (edge[0], edge[1])
 		G.layout('circo')                         # Set hierarchical layout
 		#print G.string()
 		G.draw(filename)                        # Save the image.
@@ -74,7 +79,7 @@ class Chandra:
 		
 	def start_greedy(self):
 		nodes = self.g.nodes[:]
-		print nodes
+		
 		I = sorted(nodes, reverse=True)
 		I_ = set()
 		while I:
@@ -83,26 +88,93 @@ class Chandra:
 			I_.add(heaviest)
 			for neighbor in heaviest.connections:
 				if neighbor in I:
-					print "Removing", neighbor
+					#print "Removing", neighbor
 					I.remove(neighbor)
 			#print "Remaining:"
 			#for node in I:
 				#print node
-		print I_
+		self.I = I_
 		new_graph = SetGraph()
 		new_graph.generate(list(I_))
 		new_graph.render_image("output2.png")
 		
+	def improve(self):
+		I = list(self.I)
+		ratios = [] 
+		
+		for node in I[:]:
+			subsets = []
+			for n in range(1, len(node.connections) + 1):
+				for perm in itertools.permutations(node.connections, n):
+					# for each subset element, union them one by one
+					# and observe if it doesnt grow more than K.
+					# if grows < K, it means one of its items was already in the union.
+					# so it's not an independent set. break and move to another subset.
+					
+					cumulative_items = set() 
+					for i in range(len(perm)):
+						old_length = len(cumulative_items)
+						cumulative_items.update(*perm[i].items)
+						if len(cumulative_items) - old_length < K:
+							# repeating, we have intersection so its not independent
+							#print "dependent:", perm
+							break
+					else:
+						#print "independent:",perm
+						subsets.append( perm )
+			print "Subets:"
+			for subset in subsets:
+				print subset
+			for Q in subsets:
+				"""print "Q:",
+				for n in Q:
+					print n,
+				print"""
+				w_q = 0.0
+				neighbors = []
+				for node in Q:
+					w_q += node.weight
+					neighbors.extend(node.connections) # add node's neighbors to neighbors list.
+				w_n = sum ( [ node.weight for node in set(neighbors) ] )
+				ratios.append(w_q/w_n)
+				if w_q / w_n > 1: # if W(Q) / W(Neighbors of Q) > 1:
+					# remove those adjacent to Q
+					#print "Removing neighbors of %s" %,
+					#for n in neighbors: print n,
+					print "Adding Q:",
+					for n in Q: print n,
+					print
+					for node in neighbors:
+						if node in I:
+							I.remove(node)
+					# add Q
+					I.extend(Q)
+					I = list(set(I))
+					print I
+		new_graph = SetGraph()
+		new_graph.generate(I)
+		new_graph.render_image("output3.png")
+		ratios.sort()
+		print "Biggest 10:", ratios[-10:]
+		self.I = set(I)
 
+def happiness(table):
+	"""
+	Find the happiness of the table
+	- by calculating the maximum distance between the letters
+	"""
+	return abs(ord(table[0]) - ord(table[-1]))
+	
 def generate_graph():
-	import itertools
 	nodes = []
-	for combination in itertools.combinations('abcde', r=2):
+	for combination in itertools.combinations('abcdefg', r=K):
 		#weight = sum( [ord(l) for l in combination] ) / 495.0
-		weight = random.random()
+		weight = random.randrange(100) #happiness(combination) # random.random()
 		n = Node(combination, weight )		
 		nodes.append(n)
-		
+	random.shuffle(nodes)
+	nodes = nodes[:len(nodes)/2]
+	
 
 	g = SetGraph()
 	g.generate(nodes)
@@ -111,6 +183,7 @@ def generate_graph():
 	
 	chandra = Chandra(g)
 	chandra.start_greedy()
+	chandra.improve()
 	
 if __name__ == "__main__":
 	generate_graph()
