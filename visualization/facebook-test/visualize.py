b4281c5d68c9337a20df1986245b17f830c7061c
Ahmet Emre Aladağ <aladagemre@gmail.com>
2014-09-03 16:27:49 +0300

    Trying FQL

--- visualization/facebook-test/visualize.py
+++ visualization/facebook-test/visualize.py
@@ -6,9 +6,15 @@
 >>> user = graph.get_object("239048238945720572")
 >>> user = graph.get_object("username")
 >>> friends2 = graph.get_connections("me","friends")
+SELECT name,mutual_friend_count FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me()) ORDER BY mutual_friend_count desc
+SELECT uid, name, pic_square FROM user WHERE is_app_user AND uid IN (SELECT uid2 FROM friend WHERE uid1 = me())
+
+
+SELECT uid1 FROM friend WHERE uid2 IN(SELECT name,mutual_friend_count FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me()) ORDER BY mutual_friend_count desc limit 5)
+olmadi: SELECT uid2 FROM friend WHERE uid1 IN(SELECT uid2 FROM friend WHERE uid1=me())
 """
 
-
+import os
 import facebook
 import networkx
 import matplotlib.pyplot as plt
@@ -21,7 +27,7 @@ class Person:
             self.name = unicode(x.decode("latin5").encode("utf-8"))
         elif isinstance(x, unicode):
             self.name = x
-        self.id = d.get('id')
+        self.id = d.get('uid')
 
     def __unicode__(self):
         return self.name
@@ -38,12 +44,17 @@ def get_graph(token):
     
 def my_friends(graph):
     myfrs = graph.get_connections("me", "friends")['data']
-    return [Person(person) for person in myfrs]
+    FQL = "SELECT uid,name FROM user WHERE sex IN (select sex from user where uid = me()) AND not is_app_user AND relationship_status='single' AND uid IN(SELECT uid2 FROM friend WHERE uid1=me()) ORDER BY mutual_friend_count desc;"
+    #myfrs = graph.fql(FQL)
+    #print myfrs
+    print [per.get('name') for per in myfrs]
+    #return []
+    return [Person(person) for person in myfrs if person]
 
 def my_network(graph, my_name):
     for friend in my_friends(graph):
         print (friend.name, type(friend.name))
-        G.add_edge(my_name, friend.name)
+        G.add_edge(my_name, friend.name) 
 
         mutual_friends = graph.get_connections("me", "mutualfriends/%s" % friend.id)['data']
         mutual_objects = [Person(person) for person in mutual_friends]
